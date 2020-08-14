e9b6ee65788b1ae9c778d8b8d887a5cde1af6d03
vetteaholic25 <61364594+vetteaholic25@users.noreply.github.com>
2020-08-14 17:57:58 -0400

    Add files via upload
    This was to learn how to upload a file

---
+++ Demo_02_01.py
@@ -0,0 +1,21 @@
+#######################################################
+# File:       Demo_02_01.py
+# Class:      CIS-1400
+# Chapter:    Chapter 2
+# Purpose:    Print some letters on the screen
+#
+# Concepts:
+#      o) print
+#######################################################
+
+print('AAA')
+print('BBB')
+print()  # Blank line
+print()  # Blank line
+print('CCC')
+print('DDD')
+print('')  # Blank line, alternate format
+print('EEE')
+print('FFF')
+print('GGG')
+print('HHH')
